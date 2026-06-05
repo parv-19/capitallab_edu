b810f0315c9ce712cdda3de764094ab9a0f57d88
parv-19 <parv-19@users.noreply.github.com>
2026-06-05 13:57:28 +0530

    Replace replaceAll for build compatibility

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -34,11 +34,11 @@ function extractSection(source: string, startTag: string, endTag: string) {
 
 function escapeHtml(value: string) {
   return value
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#39;");
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
 }
 
 function initials(name: string) {
